9f94fef9d40becf337e34fe9d7d8234ae9a3a177
bautista <baumancastignani@hotmail.com>
2025-06-20 19:19:45 -0300

    main limpio

---
+++ src/Game/Game.cpp
@@ -0,0 +1,52 @@
+#include "Game.h"
+#include "raylib.h"
+#include "Entitys/Player.h"
+#include "Entitys/Enemy.h"
+#include "Entitys/Bullet.h"
+#include "vector"
+
+int screenWidth = 800;
+int screenHeight = 600;
+
+std::vector<Entity::Bullet> bullets;
+
+void Game::runGame()
+{
+
+    InitWindow(screenWidth, screenHeight, "Space Invaders");
+
+    Entity::Player player;
+    Entity::Enemy enemy;
+    Entity::initPlayer(player);
+    Entity::initEnemy(enemy, screenWidth / 2, screenHeight * 0.1f);
+
+    while (!WindowShouldClose())
+    {
+        Entity::updatePlayer(player);
+        Entity::updateEnemy(enemy);
+
+        for (size_t i = 0; i < bullets.size(); i++)
+        {
+            Entity::updateBullet(bullets[i]);
+        }
+
+        BeginDrawing();
+
+        ClearBackground(BLACK);
+
+        Entity::drawPlayer(player);
+        Entity::drawEnemy(enemy);
+
+        for (size_t i = 0; i < bullets.size(); i++)
+        {
+            Entity::drawBullet(bullets[i]);
+        }
+
+        EndDrawing();
+
+    }
+
+
+    CloseWindow();
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,7 @@
-#include "raylib.h"
-#include "Entitys/Player.h"
-#include "Entitys/Enemy.h"
-#include "Entitys/Bullet.h"
-#include "vector"
-
- int screenWidth = 800;
- int screenHeight = 600;
-
- std::vector<Entity::Bullet> bullets;
+#include "Game/Game.h"
 
 int main()
 {
-
-    InitWindow(screenWidth, screenHeight, "Space Invaders");
-
-    Entity::Player player;
-    Entity::Enemy enemy;
-    Entity::initPlayer(player);
-    Entity::initEnemy(enemy, screenWidth / 2, screenHeight * 0.1f);
-    
-    while (!WindowShouldClose())    
-    {
-        Entity::updatePlayer(player);
-        Entity::updateEnemy(enemy);
-        
-        for (size_t i = 0; i < bullets.size(); i++)
-        {
-            Entity::updateBullet(bullets[i]);
-        }
-
-        BeginDrawing();
-
-        ClearBackground(BLACK);
-
-        Entity::drawPlayer(player);
-        Entity::drawEnemy(enemy);
-
-        for (size_t i = 0; i < bullets.size(); i++)
-        {
-            Entity::drawBullet(bullets[i]);
-        }
-
-        EndDrawing();
-       
-    }
-
-    
-    CloseWindow();       
-    
+    Game::runGame();
     return 0;
 }
