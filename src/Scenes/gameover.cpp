c42ea71faf928185ffbb2f779e5654a2b6e1620b
Bauman <baumancastignani@hotmail.com>
2026-08-02 18:06:32 -0300

    La pantalla de GameOver permite volver al menu principal

--- src/Scenes/gameover.cpp
+++ src/Scenes/gameover.cpp
@@ -10,6 +10,7 @@
 extern GM::gameManager gamemanager;
 
 static UI::Button play;
+static UI::Button back;
 
 static void drawGameOver()
 {
@@ -17,6 +18,7 @@ static void drawGameOver()
 	DrawText("GAME OVER", gamemanager.screenWidth / 2 - MeasureText("Game Over", 50) / 2, gamemanager.screenHeight * 0.2, 50, WHITE);
 
 	UI::drawButton(play);
+	UI::drawButton(back);
 }
 
 static void updateGameOver()
@@ -24,7 +26,16 @@ static void updateGameOver()
 	if (UI::clickButton(play))
 	{
 		gamemanager.currentScreen = scenes::Gameplay;
-		gameplay::initGamePlay();
+		gameplay::restarGamePlay();
+	}
+
+	if (UI::clickButton(back))
+	{
+		gamemanager.currentScreen = scenes::MainMenu;
+		StopMusicStream(gamemanager.music);
+		UnloadMusicStream(gamemanager.music);
+		gamemanager.music = LoadMusicStream("res/Asteroids_menu.wav");
+		PlayMusicStream(gamemanager.music);	
 	}
 }
 
@@ -44,9 +55,11 @@ void gameover::runGameOver()
 void gameover::initGameOver()
 {
 	UI::inItButton(play, gamemanager.screenWidth / 2, gamemanager.screenHeight / 2, LoadTexture("res/Play_on.png"), LoadTexture("res/Play_off.png"));
+	UI::inItButton(back, gamemanager.screenWidth / 2, gamemanager.screenHeight * 0.7, LoadTexture("res/Back_on.png"), LoadTexture("res/Back_off.png"));
 }
 
 void gameover::unloadGameOver()
 {
 	UI::unloadButton(play);
+	UI::unloadButton(back);
 }
